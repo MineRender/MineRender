5f46031655c968e31e6725c6e7bac5f502e7770b
inventivetalent <mail@inventivetalent.org>
2023-12-01 09:40:29 +0100

    dynamically import prismarine-nbt to avoid protodef breaking things when it's not even needed

--- src/nbt/NBTHelper.ts
+++ src/nbt/NBTHelper.ts
@@ -1,10 +1,10 @@
-import * as prismarineNbt from "prismarine-nbt";
 import { NBT } from "prismarine-nbt";
 import { MinecraftAsset } from "../MinecraftAsset";
 
 export class NBTHelper {
 
     public static async fromBuffer(buffer: Buffer): Promise<NBTAsset> {
+        const prismarineNbt = await import("prismarine-nbt");
         const { parsed, type, metadata } = await prismarineNbt.parse(buffer);
         return parsed;
     }
